edbfb9799ee3946dc6ec0e37e151b3fdcd913864
ghulamdastgir0 <ghulamdastgir12905@gmail.com>
2026-07-23 20:34:47 +0500

    initialize project setup

---
+++ frontend/src/routes.jsx
@@ -0,0 +1,15 @@
+import { useRoutes } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import Home from './pages/Home';
+
+const AppRoutes = () => {
+  return useRoutes([
+    {
+      path: '/',
+      element: <MainLayout />,
+      children: [{ index: true, element: <Home /> }]
+    }
+  ]);
+};
+
+export default AppRoutes;
