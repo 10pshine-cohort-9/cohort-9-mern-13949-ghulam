433dacba5b03e4af05f5cb055a41757f653809b6
ghulamdastgir0 <ghulamdastgir12905@gmail.com>
2026-07-24 02:36:41 +0500

    Auth Frontend with test

--- frontend/src/pages/Home.test.jsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Home from './Home';
-
-test('renders the home heading', () => {
-  render(<Home />);
-  expect(screen.getByRole('heading', { name: 'Home' })).toBeInTheDocument();
-});
